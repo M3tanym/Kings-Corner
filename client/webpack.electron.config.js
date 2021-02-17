d524c072f2168848d1a9ab8f314aad0db3555430
Max Rosoff <me@maxrosoff.com>
2021-02-17 15:31:58 -0700

    Try This!

--- client/webpack.electron.config.js
+++ client/webpack.electron.config.js
@@ -43,7 +43,7 @@ module.exports = {
 					[
 						{loader: 'style-loader'},
 						{loader: 'css-loader'},
-						{loader: 'postcss-loader', options: {postcssOptions: {plugins: ['autoprefixer']}}},
+						{loader: 'postcss-loader', options: {postcssOptions: {plugins: ['postcss-preset-env', 'autoprefixer']}}},
 						{loader: 'sass-loader'}
 					]
 			},
