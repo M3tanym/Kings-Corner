8497898689d7980b5f72edda825b18746d97c905
Matthew Ernst <matthew.f.ernst@gmail.com>
2021-02-17 14:03:18 -0700

    win

--- client/webpack.electron.config.js
+++ client/webpack.electron.config.js
@@ -35,7 +35,7 @@ module.exports = {
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
 				loader: 'babel-loader',
-				options: {presets: ['@babel/preset-env', '@babel/react']}
+				options: {presets: ['@babel/react']}
 			},
 			{test: /\.css$/i, use: ['style-loader', 'css-loader']},
 			{
